a7090c81bcae3f41d2224c8d309b514ce7f019b4
Vlad <bugayev@wwu.edu>
2019-09-04 14:46:26 -0700

    added functionality to the matloader function to tell the size of data from a data file

--- matloader.py
+++ matloader.py
@@ -16,7 +16,12 @@ parser.add_argument('--read_keys', action='store_true',
 # Entries in dictionary to read from for output
 parser.add_argument('--read_values', nargs='+', default=['trainingLength(seconds)', 'TotalAvgPredMSE', 'TotalAvgTrueMSE'],
                     help='entries to read in the dictionary of data stored (default=[\'trainingLength(seconds)\', \'TotalAvgPredMSE\', \'TotalAvgTrueMSE\'].\
-                    This will be overwritten if the --read_all or --read_keys flags are set')
+                    This will be overwritten if the --read_all, --read_keys, or --read_size flags are set')
+
+# Read the size of the data that the filepath is pointed to
+parser.add_argument('--read_size', action='store_true',
+                    help='Read back the size of the data that the filepath points to. Will return how many sequences are stored in'
+                         'a specific file. Will be overwritten if the --read_all, or --read_keys flags are set')
 
 args = parser.parse_args()
 
@@ -29,6 +34,15 @@ if(args.read_all):
     print(fileContents)
 elif(args.read_keys):
     print(fileContents.keys())
+elif(args.read_size):
+    if('finalStateValues' in fileContents.keys()):
+        dataSize = fileContents['finalStateValues'].shape[1]
+    elif('trueStateTEST' in fileContents.keys()):
+        dataShape = fileContents['trueStateTEST'].shape
+        dataSize = (dataShape[0], dataShape[3])
+    else:
+        raise Exception('{} is not a data file, please use a file with correct keys'.format(matFile))
+    print('there are {} sequences stored in this file'.format(dataSize))
 else:
     printList = []
     for entry in args.read_values:
