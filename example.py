e804a49e97bd63d5ccc99d2b28d758ebaa095ef3
grootva <grootva@wwu.edu>
2020-04-20 12:39:22 -0700

    not a real change, but using example.py to test out LS Functionality I havent pushed yet

--- example.py
+++ example.py
@@ -1,12 +1,32 @@
 from datagen.GaussMarkovProcess import GaussMarkovSample, GaussMarkovProcess
 from datagen.utilities import toeplitzifyData
 from datagen.GilbertElliotProcess import gilbertElliotProcess
+from LeastSquares.LeastSquares import fitLSFilter
+
+import numpy as np
 
 
-# test = GaussMarkovProcess(simuLen=10, ar_coefficients=(1.949, -0.95))
+[x, z, _, _] = GaussMarkovProcess(simuLen=20, ar_coeffs=(0.3, 0.1))
 
-# test1 = toeplitzifyData(test[0], seqLen=2)
+obsMat = toeplitzifyData(z, seqLen=10)[:, 0:-1]
+procMat = toeplitzifyData(x, seqLen=10)
+predProcVals = procMat[:, -1]
+estProcVals = procMat[:, -2]
 
-test2 = gilbertElliotProcess(simuLen=100, transProb=0.1)
+coefs1 = fitLSFilter(obsMat, predProcVals)
+
+# Verifying the functionality of the LS coeffs
+[x, z, _, _] = GaussMarkovProcess(simuLen=20, ar_coeffs=(0.3, 0.1), seed=1)
+
+obsMat = toeplitzifyData(z, seqLen=10)[:, 0:-1]
+procMat = toeplitzifyData(x, seqLen=10)
+predProcVals = procMat[:, -1]
+estProcVals = procMat[:, -2]
+
+# TODO: Figure out why this number is not converging to what we expect it to converge to
+l1 = np.mean(np.square(np.abs(np.matmul(obsMat, coefs1) - predProcVals)))
+
+
+# test2 = gilbertElliotProcess(simuLen=100, transProb=0.1)
 
 print('hello world')
