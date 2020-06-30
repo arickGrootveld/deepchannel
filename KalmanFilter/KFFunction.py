56c9b3bb24cf8b6b97520d9ba0188b315ce7bfcb
grootva <grootva@wwu.edu>
2020-06-29 23:22:51 -0700

    updated KF code have correct MSE estimate on startup

--- KalmanFilter/KFFunction.py
+++ KalmanFilter/KFFunction.py
@@ -65,10 +65,8 @@ def KFTesting(testData, ARCoeffs, debug=False, **kwargs):
             ############################# KALMAN FILTER 1  ##############################
             # This is the original Kalman Filter - does not know the actual AR coeffecients of the
             # AR processes that are input, only knows the theoretical mean values.
-
             # Formatting the measured data properly
             measuredDataComplex = measuredStateData[0, q, i] + (measuredStateData[1, q, i] * 1j)
-            
 
             # Calculating the prediction of the next state based on the previous estimate
             if q == 0:
@@ -78,7 +76,10 @@ def KFTesting(testData, ARCoeffs, debug=False, **kwargs):
 
             # Calculating the predicted MSE from the current MSE, the AR Coefficients,
             # and the covariance matrix
-            minPredMSE[:, :, q, i] = np.matmul(np.matmul(F, minMSE[:, :, q - 1, i]), np.transpose(F)) + Q
+            if(i+q == 0):
+                minPredMSE[:, :, q, i] = np.matmul(np.matmul(F, minMSE[:, :, q, i]), np.transpose(F)) + Q
+            else:
+                minPredMSE[:, :, q, i] = np.matmul(np.matmul(F, minMSE[:, :, q - 1, i]), np.transpose(F)) + Q
 
             # Calculating the new Kalman gain
             intermediate1 = np.matmul(minPredMSE[:, :, q, i], np.transpose(H))
@@ -99,8 +100,6 @@ def KFTesting(testData, ARCoeffs, debug=False, **kwargs):
             minMSE[:, :, q, i] = np.matmul(intermediate1, minPredMSE[:, :, q, i])
             
 
-
-
         ############################# KALMAN FILTER 1  ##############################
 
         
@@ -113,6 +112,7 @@ def KFTesting(testData, ARCoeffs, debug=False, **kwargs):
 
         finalPrediction = np.matmul(F, x_correction[:, q, i])[0]
         finaEstimate = x_correction[:, q, i][0]
+        
 
         # Calculating the instantaneous MSE of our estimate and prediction
         trueEstimateMSE = np.absolute(finaEstimate - currentTrueStateComplex) ** 2
@@ -155,13 +155,17 @@ def KFTesting2(testData, ARCoeffs, debug=False, **kwargs):
     
     measuredStateDataTest = np.empty((2, measuredStateData.shape[1] + 
         measuredStateData.shape[2]))
-    print(measuredStateDataTest.shape)
-    print(measuredStateData.shape)
-    print(trueStateData.shape)
-    
     
     # Variables for deciding length of variables
-    seriesLength = trueStateData.shape[2]
+    seriesLength = trueStateData.shape[1]
+
+
+    # Looping through data to grab the relevant samples
+    for p in range(0, seriesLength):
+        if p == 0:
+            measuredStateDataTest[:, 0:10] = measuredStateData[:, :, 0]
+        else:
+            measuredStateDataTest[:, 9 + p] = measuredStateData[:, 9, p]
 
     ##### Kalman Filter Implementation #####
     # Initializing the Kalman Filter variables
@@ -171,18 +175,18 @@ def KFTesting2(testData, ARCoeffs, debug=False, **kwargs):
     # Prediction/estimate formatted into current and last state value in the 1st dimension,
     # by sequence in the 2nd dimension, and by series in the 3rd dimension
     x_correction = np.zeros((AR_n, 
-                             seriesLength), dtype=complex)
+                             seriesLength+10), dtype=complex)
     x_prediction = np.zeros((AR_n, 
-                             seriesLength), dtype=complex)
+                             seriesLength+10), dtype=complex)
 
     # Other Kalman Filter variables formatted into a matrix/vector of values in the 1st and 2nd dimensions,
     # by sequence in the 3rd dimension, and by series in the 4th dimension
     kalmanGain = np.zeros((AR_n, 1, 
-                           seriesLength))
+                           seriesLength+10))
     minPredMSE = np.zeros((AR_n, AR_n, 
-                           seriesLength))
+                           seriesLength+10))
     minMSE = np.zeros((AR_n, AR_n, 
-                       seriesLength))
+                       seriesLength+10))
 
     # Initializing the correction value to be the expected value of the starting state
     x_correction[:, 0] = np.array([0, 0])
@@ -209,69 +213,78 @@ def KFTesting2(testData, ARCoeffs, debug=False, **kwargs):
     
     if(debug):
         instaErrs = np.empty([1, seriesLength])
+        kfPreds = np.empty([1, seriesLength], dtype=np.complex128)
 
-    for i in range(0, seriesLength):
+    for i in range(0, seriesLength + 10):
         # Loop through a sequence of data
         #############################################################################
         ############################# KALMAN FILTER 1  ##############################
         # This is the original Kalman Filter - does not know the actual AR coeffecients of the
         # AR processes that are input, only knows the theoretical mean values.
-
         # Formatting the measured data properly
-        measuredDataComplex = measuredStateData[0, q, i] + (measuredStateData[1, q, i] * 1j)
-
+        measuredDataComplex = measuredStateDataTest[0, i] + (measuredStateDataTest[1, i] * 1j)
+        
         # Calculating the prediction of the next state based on the previous estimate
-        x_prediction[:, q, i] = np.matmul(F, x_correction[:, q - 1, i])
+        if i > 0:
+            x_prediction[:, i] = np.matmul(F, x_correction[:, i-1])
 
         # Calculating the predicted MSE from the current MSE, the AR Coefficients,
         # and the covariance matrix
-        minPredMSE[:, :, q, i] = np.matmul(np.matmul(F, minMSE[:, :, q - 1, i]), np.transpose(F)) + Q
-
+        if i > 0:
+            minPredMSE[:, :, i] = np.matmul(np.matmul(F, minMSE[:, :, i-1]), np.transpose(F)) + Q
+        else:
+            minPredMSE[:, :, i] = np.matmul(np.matmul(F, minMSE[:, :, 0]), np.transpose(F)) + Q
         # Calculating the new Kalman gain
-        intermediate1 = np.matmul(minPredMSE[:, :, q, i], np.transpose(H))
+        intermediate1 = np.matmul(minPredMSE[:, :, i], np.transpose(H))
         # Intermediate2 should be a single dimensional number, so we can simply just divide by it
-        intermediate2 = np.linalg.inv(R + np.matmul(np.matmul(H, minPredMSE[:, :, q, i]),
+        intermediate2 = np.linalg.inv(R + np.matmul(np.matmul(H, minPredMSE[:, :, i]),
                                                         np.transpose(H)))
-        kalmanGain[:, :, q, i] = np.matmul(intermediate1, intermediate2)
+        kalmanGain[:, :, i] = np.matmul(intermediate1, intermediate2)
 
         # Calculating the State Correction Value
-        intermediate1 = np.matmul(H, x_prediction[:, q, i])
+        intermediate1 = np.matmul(H, x_prediction[:, i])
         intermediate2 = measuredDataComplex - intermediate1
-        x_correction[:, q, i] = x_prediction[:, q, i] + np.matmul(kalmanGain[:, :, q, i],
+        x_correction[:, i] = x_prediction[:, i] + np.matmul(kalmanGain[:, :, i],
                                                                       intermediate2)
         # Calculating the MSE of our current state estimate
-        intermediate1 = np.identity(AR_n) - np.matmul(kalmanGain[:, :, q, i], H)
-        minMSE[:, :, q, i] = np.matmul(intermediate1, minPredMSE[:, :, q, i])
+        intermediate1 = np.identity(AR_n) - np.matmul(kalmanGain[:, :, i], H)
+        minMSE[:, :, i] = np.matmul(intermediate1, minPredMSE[:, :, i])
 
         ############################# KALMAN FILTER 1  ##############################
 
-        
+                
         # Updating the correction value to persist between rows, as the Kalman Filter
         # is an iterative approach, not having this persistence causes it to take 
         # longer to converge to the Riccati equation than it should
+        if(i >= 10):
 
-        ## Calculating the actual MSE between the kalman filters final prediction, and the actual value ##
-        # Converting the true states into their complex equivalents
-    currentTrueStateComplex = trueStateData[0, i] + (1j * trueStateData[2, i])
-    nextTrueStateComplex = trueStateData[1, i] + (1j * trueStateData[3, i])
-        
+            ## Calculating the actual MSE between the kalman filters final prediction, and the actual value ##
+            # Converting the true states into their complex equivalents
+            currentTrueStateComplex = trueStateData[0, i-10] + (1j * trueStateData[2, i-10])
+            nextTrueStateComplex = trueStateData[1, i-10] + (1j * trueStateData[3, i-10])
 
-    finalPrediction = np.matmul(F, x_correction[:, q, i])[0]
-    finaEstimate = x_correction[:, q, i][0]
+            finalPrediction = np.matmul(F, x_correction[:, i])[0]
+            finalEstimate = x_correction[:, i][0]
 
-    # Calculating the instantaneous MSE of our estimate and prediction
-    trueEstimateMSE = np.absolute(finaEstimate - currentTrueStateComplex) ** 2
-    truePredictionMSE = np.absolute(finalPrediction - nextTrueStateComplex) ** 2
+            # Calculating the instantaneous MSE of our estimate and prediction
+            trueEstimateMSE = np.absolute(finalEstimate - currentTrueStateComplex) ** 2
+            truePredictionMSE = np.absolute(finalPrediction - nextTrueStateComplex) ** 2
 
-    # If Debugging, then grab the instantaneous errors
-    if debug:
-        instaErrs[0, i] = truePredictionMSE
 
-    totalTrueEstimateMSE += trueEstimateMSE
-    totalTruePredMSE += truePredictionMSE
+            if debug:
+                instaErrs[0, i-10] = truePredictionMSE
+                kfPreds[0, i-10] = finalPrediction
+
+            totalTrueEstimateMSE += trueEstimateMSE
+            totalTruePredMSE += truePredictionMSE
 
     totalTrueEstimateMSE = totalTrueEstimateMSE / (seriesLength)
     totalTruePredMSE = totalTruePredMSE / (seriesLength)
+    
 
-    return (totalTrueEstimateMSE, totalTruePredMSE)
+    # Different return pattern if we are in debug or not
+    if debug:
+        return (totalTrueEstimateMSE, totalTruePredMSE, instaErrs, kfPreds)
+    else:
+        return (totalTrueEstimateMSE, totalTruePredMSE)
 
