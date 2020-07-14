646667cf5911e384bc868ac91c714e6e504e083b
grootva <grootva@wwu.edu>
2020-07-14 09:59:16 -0700

    added code to convert data from Maneuvering Targets code to a format familiar to the TCN, including test, train and eval data

---
+++ CoordinatedTurn/c_turn.m
@@ -0,0 +1,30 @@
+% Co-ordinated turn model
+function [f2, F_transition] = c_turn(x,param)
+    dt = param;
+    if x(5) == 0
+        f2 =     [x(1) + x(3) * dt;
+                  x(2) + x(4) * dt;
+                  x(3);
+                  x(4);
+                  0              ];
+         F_transition = [1, 0, dt, 0;
+                         0, 1, 0, dt;
+                         0, 0, 1, 0;
+                         0, 0, 0, 1];
+        %  test = F_transition * x(1:4);
+    else
+        wt = x(5) * dt;
+        w = x(5);
+        f2 =     [x(1) + x(3)/w * sin(wt) - x(4)/w * (1 - cos(wt));
+                  x(2) + x(3)/w* (1 - cos(wt)) + x(4)/w*sin(wt);
+                  x(3)*cos(wt) - x(4) * sin(wt);
+                  x(3) * sin(wt) + x(4)* cos(wt);
+                  w];
+        F_transition = [1, 0, 1/w * sin(wt), -1/w * (1 - cos(wt));
+                        0, 1, 1/w * (1-cos(wt)), 1/w*sin(wt);
+                        0, 0, cos(wt), -1*sin(wt);
+                        0, 0, sin(wt), cos(wt)];
+     %   test = F_transition * x(1:4);
+    end
+
+end 
---
+++ CoordinatedTurn/utilities/reformattingManTargData.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+def batchingTestData(trueStates, obsStates, batchSize):
+    trueStatesShape = trueStates.shape
+    obsStatesShape = obsStates.shape
+    # Calculating the number of batches from the data.
+    # We cut off the remaining data after fitting the maximum
+    # number of batches that evenly divide the dataset
+    numBatches = int(np.floor(trueStatesShape[2] / args.batchSize))
+    
+    
+    trueStatesBatched = np.zeros((trueStatesShape[0], batchSize, \
+                                  trueStatesShape[1], numBatches))
+
+    obsStatesBatched = np.zeros((obsStatesShape[0], batchSize, \
+                                obsStatesShape[1], obsStatesShape[2], \
+                                numBatches))
+
+    for i in range(0, numBatches):
+        for m in range(0, batchSize):
+            trueStatesBatched[:, m, :, i] = trueStates[:, :, (batchSize * i) + m]
+
+            obsStatesBatched[:, m, :, :, i] = obsStates[:, :, :, (batchSize * i) + m]
+    
+    return(trueStatesBatched, obsStatesBatched)
+
+if __name__ == '__main__':
+    import hdf5storage as h5
+    import argparse
+    import os
+    parser = argparse.ArgumentParser(description="""Reformatting .mat data 
+                                                    generated
+                                                    by matlab to work
+                                                    with our TCN code""")
+    parser.add_argument('--targFile', type=str, default='None', metavar='t',
+                        help='The file to reformat and save over')
+
+    parser.add_argument('--testData', action='store_true',
+                        help='convert to test data (default: False)')
+
+    parser.add_argument('--batchSize', type=int, default=50, metavar='N',
+                        help='The size of the batches generated (default: 50)')
+
+
+    args = parser.parse_args()
+
+    targetFile = args.targFile
+
+    print('Loading data from: ', targetFile)
+
+    matlabFormattedData = h5.loadmat(targetFile)
+
+    runData = dict()
+
+    # If the data is already reformatted, then we can simply inform the user
+    # that this is the case and leave the data alone
+    if 'reformattedData' in matlabFormattedData.keys():
+        runData = matlabFormattedData
+        print('data in: ' + targetFile + ' is already reformatted')
+    else:
+
+        os.remove(targetFile)
+
+        # If we are generating test data, then we need to have a slightly
+        # different format for the data
+        if(args.testData):
+            runData['reformattedData'] = True
+
+            # Rewriting the data to a test data file
+            inter1 = targetFile.rfind('/')
+            inter2 = targetFile.find('ManTargData')
+            inter3 = targetFile.rfind('.')
+            targetFile = targetFile[0:inter1 + 1] + 'ManTargTestData' \
+                + targetFile[inter2 + 11:inter3] + '.mat'
+            
+            allTrueStates = matlabFormattedData['data'][0,0]['systemStates']
+            # inter1 = LSAndKFTestData.shape
+            # runData['LSandKFTestData'] = np.reshape(LSAndKFTestData, (1, inter1[0], \
+                                                    # inter1[1], inter1[2]))
+
+            measInter1 = matlabFormattedData['data'][0,0]['observedStates']
+            inter2 = measInter1.shape
+            # runData['measuredStateTEST'] = np.reshape(inter1, (1, inter2[0], inter2[1], \
+                                                    #   inter2[2]))
+            measStateTest = np.reshape(measInter1, (1, inter2[0], inter2[1], \
+                                                    inter2[2]))
+
+            trueInter1 = matlabFormattedData['data'][0,0]['finalStateValues']
+            inter2 = trueInter1.shape
+            # runData['trueStateTEST'] = np.reshape(inter1, (1, inter2[0], \
+                                                #  inter2[1]))
+            trueStateTest = np.reshape(trueInter1, (1, inter2[0], \
+                                                inter2[1]))
+
+            # Before batching it, we save it for the LSandKFTestData variable
+            runData['LSandKFTestData'] = [[allTrueStates, measInter1, trueInter1]]
+
+            [trueStateTest, measStateTest] = batchingTestData(trueStateTest, \
+                                                            measStateTest, \
+                                                            args.batchSize)
+
+            runData['trueStateTEST'] = trueStateTest
+            runData['measuredStateTEST'] = measStateTest
+
+            inter1 = dict()
+            inter1['riccatiConvergenceEst'] = 0.00
+            inter1['riccatiConvergencePred'] = 0.00
+            inter1['seed'] = 0
+            runData['testDataInfo'] = [inter1]
+
+            # TODO: Batch the test data so it has same format as normal files
+        else:
+            runData['seed'] = matlabFormattedData['data'][0,0]['seed'][0][0]
+            runData['finalStateValues'] = matlabFormattedData['data'][0,0]['finalStateValues']
+            runData['observedStates'] = matlabFormattedData['data'][0,0]['observedStates']
+            runData['systemStates'] = matlabFormattedData['data'][0,0]['systemStates']
+
+            # Intermediate variable to hold the parameters so they can be 
+            # reorganized in usable fashion
+            intermed = dict()
+            intermed['numSequences'] = matlabFormattedData['data'][0,0]['parameters'][0]['numSequences'][0][0][0]
+            intermed['sequenceLength'] = matlabFormattedData['data'][0,0]['parameters'][0]['sequenceLength'][0][0][0]
+            # Saving the parameters to data to be saved
+            runData['parameters'] = intermed
+
+            runData['riccatiConvergences'] = matlabFormattedData['data'][0,0]['riccatiConvergences']
+            runData['reformattedData'] = True
+        print('saving data to: ' + targetFile)
+
+        # Saving the data to its new file
+        h5.savemat(targetFile, runData)
+
+
+
+
+        
+
